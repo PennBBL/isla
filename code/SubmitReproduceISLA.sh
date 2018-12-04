a84ecc82b0424db7da445d99f3ff85fed09d8087
Tinashe Tapera <ttapera@chead.uphs.upenn.edu>
2018-12-04 15:25:33 -0500

    fixed Singularity Submit; imco() still doesn't run correctly for n=4

--- code/SubmitReproduceISLA.sh
+++ code/SubmitReproduceISLA.sh
@@ -1,2 +1,2 @@
-/share/apps/singularity/2.5.1/bin/singularity exec /data/joy/BBL/applications/bids_apps/imco.simg Rscript ReproduceISLA.R 3
-/share/apps/singularity/2.5.1/bin/singularity exec /data/joy/BBL/applications/bids_apps/imco.simg Rscript ReproduceISLA.R 4
+/share/apps/singularity/2.5.1/bin/singularity exec -B /data:/home/ttapera/data /data/joy/BBL/applications/bids_apps/imco.simg Rscript /home/ttapera/data/jux/BBL/projects/isla/code/ReproduceISLA.R 3
+/share/apps/singularity/2.5.1/bin/singularity exec -B /data:/home/ttapera/data /data/joy/BBL/applications/bids_apps/imco.simg Rscript /home/ttapera/data/jux/BBL/projects/isla/code/ReproduceISLA.R 4
